e8620dc9fd8230221cca276026a1ed029b682f71
SelfwareEngineer <126501922+SelfwareEngineer@users.noreply.github.com>
2024-06-28 16:53:03 -0400

    Solve exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let splitString = str.split("");
+  let reversedString = "";
+  const splitStringLength = splitString.length;
+  for (let i = 0; i < splitStringLength; i++) {
+    reversedString += splitString.pop();
+  }
+  return reversedString;
 };
 
 // Do not edit below this line
