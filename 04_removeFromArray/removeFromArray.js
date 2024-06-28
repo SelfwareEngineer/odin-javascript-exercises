3ca7b953167875d7f4807e7d7ee3653ef35f9983
SelfwareEngineer <126501922+SelfwareEngineer@users.noreply.github.com>
2024-06-28 17:19:55 -0400

    Solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...nums) {
+  for (let num of nums) {
+    for (let item of arr) {
+      if (num === item) {
+        arr = arr.filter((item) => item != num);
+      }
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
