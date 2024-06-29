4d41feb19b9734642ba507e456891f3407130923
SelfwareEngineer <126501922+SelfwareEngineer@users.noreply.github.com>
2024-06-29 12:20:38 -0400

    Add first two parts of exercise

--- 13_articleExercise/script.js
+++ 13_articleExercise/script.js
@@ -1,2 +1,18 @@
 const container = document.querySelector("#container");
 
+// 1.
+let redP = document.createElement("p");
+redP.style.color = "red";
+redP.textContent = "Hey I'm red!";
+container.appendChild(redP);
+
+// 2.
+let blueH3 = document.createElement("h3");
+blueH3.style.color = "blue";
+blueH3.textContent = "I'm a blue h3!";
+container.appendChild(blueH3);
+
+// 3.
+
+// create and append container div first to avoid using `innerHTML()`
+let subContainer;
