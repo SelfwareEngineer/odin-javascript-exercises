c99ecf714e260c2caa1b4ab10f4fffaef0672e02
SelfwareEngineer <126501922+SelfwareEngineer@users.noreply.github.com>
2024-06-29 16:05:06 -0400

    Complete part 3 of exercise

--- 15_MDNDOM1/script.js
+++ 15_MDNDOM1/script.js
@@ -6,3 +6,9 @@ const sect = document.querySelector("section");
 const para = document.createElement("p");
 para.textContent = "We hope you enjoyed the ride.";
 sect.appendChild(para);
+
+const text = document.createTextNode(
+  " - the premier source for web development knowledge.",
+);
+const linkPara = document.querySelector("p");
+linkPara.appendChild(text);
