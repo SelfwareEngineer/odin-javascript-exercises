a7f28d0c07146d9b8838327bf43c0a69894921c5
SelfwareEngineer <126501922+SelfwareEngineer@users.noreply.github.com>
2024-07-03 17:31:55 -0400

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num < 0) {
+    return "OOPS";
+  }
 
+  let operand1 = 0;
+  let operand2 = 1;
+  let total = 0;
+
+  for (let i = 0; i < num; i++) {
+    total = operand1 + operand2;
+    operand1 = operand2;
+    operand2 = total;
+    console.log(`O1: ${operand1}, O2: ${operand2}, T: ${total}`);
+  }
+
+  return operand1;
 };
 
 // Do not edit below this line
