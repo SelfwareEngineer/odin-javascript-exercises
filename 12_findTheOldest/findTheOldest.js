0d8ed1b047acfdbaab978e2a0ac9cb9bc76d9905
SelfwareEngineer <126501922+SelfwareEngineer@users.noreply.github.com>
2024-07-03 18:17:20 -0400

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (personObjArr) {
+  let oldest = 0;
+  let index;
 
+  for (let i = 0; i < personObjArr.length; i++) {
+    if (personObjArr[i].yearOfDeath) {
+      endYear = personObjArr[i].yearOfDeath;
+    } else {
+      endYear = new Date().getFullYear();
+    }
+
+    const age = endYear - personObjArr[i].yearOfBirth;
+    if (age > oldest) {
+      oldest = age;
+      index = i;
+    }
+  }
+
+  return personObjArr[index];
 };
 
 // Do not edit below this line
