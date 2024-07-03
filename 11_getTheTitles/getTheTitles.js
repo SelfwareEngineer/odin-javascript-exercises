153316307d200e33a6ef5a5a7c2e8589f6217522
SelfwareEngineer <126501922+SelfwareEngineer@users.noreply.github.com>
2024-07-03 17:49:34 -0400

    Solve exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,5 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (bookObjArr) {
+  return bookObjArr.reduce((acc, i) => acc.concat(i.title), []);
 };
 
 // Do not edit below this line
