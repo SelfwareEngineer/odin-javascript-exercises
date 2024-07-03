f3a4f028d8e25e0e5002af6695ae590c0a6189d7
SelfwareEngineer <126501922+SelfwareEngineer@users.noreply.github.com>
2024-07-03 17:10:43 -0400

    Solve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (palindrome) {
+  // Palindromes don't care about caps, word breaks, or punctuation
+  const palindromeBlob = palindrome.toLowerCase().split(" ").join("");
+  const filteredBlob = palindromeBlob
+    .split("")
+    .filter((i) => ![".", ",", "!", '"', "'", "?", "(", ")"].includes(i))
+    .join("");
+  console.log("forwards: ", filteredBlob);
 
+  const reversedBlob = filteredBlob.split("").reverse().join("");
+  console.log("backwards: ", reversedBlob);
+
+  if (filteredBlob === reversedBlob) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
